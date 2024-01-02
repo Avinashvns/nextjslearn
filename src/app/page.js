1df762f293d713006b412ecbf61ee48c8c25b0dc
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-02 14:28:58 +0530

    use client event

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,13 @@
-
+'use client'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
-      <User name="Rani" />
+      <h4>Event ,function and State</h4>
+
+      <button onClick={() => alert("Hello Next Js")}>Click me</button>
     </main>
   )
-}
-
-
-const User = (props) => {
-  return (
-    <div>
-      <h2>User name is {props.name}</h2>
-    </div>
-  );
 }
