77e8fdc6eba916562ac2991e20bb02c98d8b8343
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-02 14:32:51 +0530

    use function

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,17 @@
 import styles from './page.module.css'
 
 export default function Home() {
+  // create function
+  const apple = () => {
+    alert("friut");
+  }
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
       <h4>Event ,function and State</h4>
 
-      <button onClick={() => alert("Hello Next Js")}>Click me</button>
+      {/* <button onClick={() => alert("Hello Next Js")}>Click me</button> */}
+      <button onClick={apple}>Click me</button>
     </main>
   )
 }
