8fb52490b773b08d9015fb76587573523ca7e0b3
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-02 14:16:32 +0530

    create component and uses

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
+      <User />
     </main>
   )
+}
+
+
+const User = () => {
+  return (
+    <div>
+      <h2>User name is Avinash</h2>
+    </div>
+  );
 }
