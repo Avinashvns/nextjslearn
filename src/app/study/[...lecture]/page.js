2f99d67e71149cd07e5bcfa888515a8afe3b8a2c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-03 11:56:50 +0530

    catch all segment of routing (get routing)

--- src/app/study/[...lecture]/page.js
+++ src/app/study/[...lecture]/page.js
@@ -1,9 +1,10 @@
+'use client'
 
-
-export default function Lecture() {
+export default function Lecture({ params }) {
     return (
         <div>
-            <h1>Lecture 1</h1>
+            <h1>Day : {params.lecture[0]}</h1>
+            <h1>Lecture : {params.lecture[1]}</h1>
         </div>
     )
 }
