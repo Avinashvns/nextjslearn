6e407b309249f6919ede06cf2d00d125f31b9d6c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-03 10:42:15 +0530

    common layout

---
+++ src/app/about/layout.js
@@ -0,0 +1,12 @@
+
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <h1>Common Layout Apply on about page and its nested roots</h1>
+            {
+                children
+            }
+        </div>
+    )
+}
