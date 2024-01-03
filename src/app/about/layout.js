857667e18658fe0aafa810117d07c9f2a4100d06
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-03 11:18:50 +0530

    condition layout ( usePathname ) hook

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,23 +1,32 @@
+'use client'
 import Link from "next/link";
+
 import './about.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
+    const pathName = usePathname();
+    console.log(pathName);
     return (
         <div>
-            <ul className="about-menu">
-                <li>
-                    <h4>About Navbar</h4>
-                </li>
-                <li>
-                    <Link href='/about'>About Page</Link>
-                </li>
-                <li>
-                    <Link href='/about/aboutcollege'>About College</Link>
-                </li>
-                <li>
-                    <Link href='/about/aboutstudent'>About Student</Link>
-                </li>
-            </ul>
+            {
+                pathName !== '/about/aboutstudent' ?
+                    <ul className="about-menu">
+                        <li>
+                            <h4>About Navbar</h4>
+                        </li>
+                        <li>
+                            <Link href='/about'>About Page</Link>
+                        </li>
+                        <li>
+                            <Link href='/about/aboutcollege'>About College</Link>
+                        </li>
+                        <li>
+                            <Link href='/about/aboutstudent'>About Student</Link>
+                        </li>
+                    </ul>
+                    : null
+            }
             {
                 children
             }
