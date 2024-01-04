af21fb9488ff65829522492e88809f18769f1365
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 12:28:38 +0530

    fetch api in client component

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,11 +1,38 @@
+"use client"
 import Link from "next/link";
-
-
+import { useState, useEffect } from "react";
 
 export default function Product() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let data = await fetch("https://dummyjson.com/products");
+                data = await data.json();
+                // console.log(data);
+                setProducts(data.products);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log("Data : ", products)
     return (
         <div>
             <h1>Product List</h1>
+
+            {
+                products.map((item, index) => (
+                    <div>
+                        <h3 >Name : {item.title}</h3>
+                        <h3 >Caterogy : {item.category}</h3>
+                    </div>
+                ))
+            }
             <Link href="/">Go to Home page</Link>
         </div>
     )
