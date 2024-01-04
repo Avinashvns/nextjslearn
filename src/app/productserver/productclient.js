b70b2cf01f59364fee2b194c76225d3c205da052
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 13:06:56 +0530

    create a client component

---
+++ src/app/productserver/productclient.js
@@ -0,0 +1,11 @@
+
+
+'use client'
+
+export default function ProductClient() {
+    return (
+        <div>
+            <button onClick={() => alert("Hi")}>Click me</button>
+        </div>
+    )
+}
