e0360bd957b77640063bba4069e466313703fff2
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 12:49:45 +0530

    fetch api in server component

--- src/app/productserver/page.js
+++ src/app/productserver/page.js
@@ -1,10 +1,27 @@
 import Link from "next/link";
 
-export default function ProductServer() {
+async function productList() {
+    let data = await fetch("https://dummyjson.com/products")
+    data = await data.json();
+    return data.products;
+}
+
+export default async function ProductServer() {
+    let products = await productList();
+    console.log(products)
     return (
         <div>
             <h1>Product List Api Call in Server Component</h1>
-
+            <br />
+            {
+                products.map((item, index) => (
+                    <div>
+                        <h3 key={index}>Name : {item.title}</h3>
+                        <h4>Category :{item.category}</h4>
+                        <br />
+                    </div>
+                ))
+            }
             <Link href='/'>Go to Main Page</Link>
         </div>
     )
