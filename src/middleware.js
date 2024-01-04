8c51d161827963c454547a3e0035a2f5595cb319
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 11:16:10 +0530

    middleware config page apply condition

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,15 @@ import { NextResponse } from "next/server"
 export default function Middleware(request) {
     // console.log(request)
 
-    if (request.nextUrl.pathname != "/login") {
-        return NextResponse.redirect(new URL("/login", request.url))
-    }
+    // if (request.nextUrl.pathname != "/login") {
+    //     return NextResponse.redirect(new URL("/login", request.url))
+    // }
 
-} 
+    // only about page redirect in login page
+    return NextResponse.redirect(new URL("/login", request.url))
+
+}
+
+export const config = {
+    matcher: "/about/:path*"
+}
