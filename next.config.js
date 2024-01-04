02b51e0bff9beb45ce8e45f7e7460eaeeaf6593d
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 14:00:20 +0530

    URL image with next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "en.wikipedia.org"
+        ]
+    }
+}
 
 module.exports = nextConfig
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -16,7 +16,7 @@ const Login = () => {
             <h2 className={custom.main}>Custom Module uses</h2>
             <Image src={Profile} />
             <br />
-            <img src={Profile.src} />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1024px-Image_created_with_a_mobile_phone.png" />
         </diV>
     )
 }
