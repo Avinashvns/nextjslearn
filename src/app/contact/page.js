d0799ab7ed8e4074396bd5ed2ccee1ca60fbe3c9
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 15:08:24 +0530

    Script Component with get location

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,15 +1,16 @@
-
+"use client"
+import Script from "next/script";
 
 export default function Contact() {
     return (
         <div>
             <h1>Contact page</h1>
+            <Script src='/location.js'
+                onLoad={() => {
+                    console.log("File Loaded")
+                }} />
+            <h1>Get User getLocation</h1>
+
         </div>
     )
-}
-
-export function generateMetadata() {
-    return {
-        title: "Contact Page",
-    };
 }
