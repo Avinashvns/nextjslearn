a2dcd02a309ac21ecf437f5263fd2f2324bbbef6
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-04 13:51:37 +0530

    Image with next js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,14 +2,19 @@
 import { useState } from "react";
 import custom from '../custom.module.css'
 
+import Image from "next/image"
+import Profile from '../../../public/vercel.svg'
+
 
 const Login = () => {
+    console.log(Profile)
     const [hstyle, setHstyle] = useState({ backgroundColor: 'yellow' })
     return (
         <diV>
             <h1 style={hstyle}>Login Page</h1>
 
             <h2 className={custom.main}>Custom Module uses</h2>
+            <Image src={Profile} />
         </diV>
     )
 }
