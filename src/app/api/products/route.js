d3afd468b2096294524144f6f8cf73cc391257fe
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-06 22:15:50 +0530

    mangoose connect

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,10 @@
+
+import { connectionSrt } from "@/mango/mango_db";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    console.log("Con :", connectionSrt)
+    // await mongoose.connect(connectionSrt)
     return NextResponse.json({ result: true })
 }
--- src/app/mango/mango_db.js
+++
@@ -1,3 +0,0 @@
-
-const { username, password } = process.env
-export const connectionSrt = "mongodb+srv://" + username + ":" + password + "@cluster0.ubcntsf.mongodb.net/productDB?retryWrites=true&w=majority"
