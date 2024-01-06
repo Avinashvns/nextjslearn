8230289c07edc8b0b0ee61ce1596f2046aa82462
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-06 15:51:12 +0530

    if user is not found

--- src/app/dj/[djid]/page.js
+++ src/app/dj/[djid]/page.js
@@ -8,35 +8,39 @@ export default async function DJPage({ params }) {
     console.log(dj)
 
     console.log(params)
+    console.log("id= ", params.djid)
+    console.log("dj id =", dj.id)
 
     return (
         <div >
             <h1>DJ Single User Data Display</h1>
             <br />
             <br />
-            <div style={{ display: 'flex', justifyContent: "space-around" }}>
-                <div>
-                    <h1>Dj User Id</h1>
-                    <h2>{dj.id}</h2>
-                </div>
-                <div>
-                    <h1>Dj Name</h1>
-                    <h2>{dj.dj_name}</h2>
-                </div>
-                <div>
-                    <h1>Dj Owner</h1>
-                    <h2>{dj.owner}</h2>
-                </div>
-                <div>
-                    <h1>Palce</h1>
-                    <h2>{dj.place}</h2>
-                </div>
-                <div>
-                    <h1>Dj Price</h1>
-                    <h2>{dj.price}</h2>
-                </div>
+            {
+                params.djid != dj.id ? "No Data" : <div style={{ display: 'flex', justifyContent: "space-around" }}>
+                    <div>
+                        <h1>Dj User Id</h1>
+                        <h2>{dj.id}</h2>
+                    </div>
+                    <div>
+                        <h1>Dj Name</h1>
+                        <h2>{dj.dj_name}</h2>
+                    </div>
+                    <div>
+                        <h1>Dj Owner</h1>
+                        <h2>{dj.owner}</h2>
+                    </div>
+                    <div>
+                        <h1>Palce</h1>
+                        <h2>{dj.place}</h2>
+                    </div>
+                    <div>
+                        <h1>Dj Price</h1>
+                        <h2>{dj.price}</h2>
+                    </div>
 
-            </div>
+                </div>
+            }
 
 
         </div>
