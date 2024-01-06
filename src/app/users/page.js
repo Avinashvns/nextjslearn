133cc1b1b3fac1ddd28b897eda24a3e5b1b7ef5b
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-06 13:30:38 +0530

    fetch data from local get api (name with age)

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,21 +1,42 @@
 
 import Link from "next/link";
-import getUsers from '../../../services/getUsers'
+// import getUsers from '../../../services/getUsers'
+
+
+// Api call from next api create files
+async function getUsers() {
+    let result = await fetch("http://localhost:3000/api/users");
+    result = await result.json();
+    return result;
+}
 
 export default async function Page() {
-    const getUserList = getUsers();
-    const users = await getUserList;
-    console.log(users)
+    const data = await getUsers();
+    console.log(data);
+    // const getUserList = getUsers();
+    // const users = await getUserList;
+    // console.log(users)
+
     return (
+
         <div>
             <h1>User List</h1>
-            {
+            {/* {
                 users.map((user) => (
                     <h2 key={user.id}>
                         <Link href={`/users/${user.id}`}>
                             {user.name}
                         </Link>
                     </h2>
+                ))
+            } */}
+            {
+                data.map((items, index) => (
+                    <div key={index}>
+                        <h1>Name: {items.name}</h1>
+                        <h1>Age: {items.age}</h1>
+                        <br />
+                    </div>
                 ))
             }
         </div>
