e32dba3f6686f56fc16e33ab53edb5c09bbf1580
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-06 13:20:52 +0530

    no user found in get api

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -7,6 +7,7 @@ export function GET(request, content) {
     console.log(content.params.id)
     const data = user;
     const userData = data.filter((item) => item.id == content.params.id)
-    return NextResponse.json(userData)
+    return NextResponse.json(
+        userData.length == 0 ? { result: "No Data Found" } : { result: userData })
 }
 
