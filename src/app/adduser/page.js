1aeab80460edfa74725f18df025151f9ebb83f19
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-06 18:24:18 +0530

    data show in console in call POST API

--- src/app/adduser/page.js
+++ src/app/adduser/page.js
@@ -1,9 +1,27 @@
+"use client"
 
+import { useState } from "react"
 
 export default function AddUser() {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+
+    const addUser = () => {
+        console.log("hi")
+        console.log(name, age, gender)
+    }
+
     return (
         <div>
             <h1>Add User</h1>
+            <input type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
+            <br />
+            <input type="number" placeholder="Enter Age" value={age} onChange={(e) => setAge(e.target.value)} />
+            <br />
+            <input type="text" placeholder="Enter Gender" value={gender} onChange={(e) => setGender(e.target.value)} />
+            <br />
+            <button onClick={addUser} >Add User</button>
         </div>
     )
 }
