55c6c749d79bd3b7c9e502033160c7e72bacded8
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-07 00:23:45 +0530

    get api with mango_db

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 
 // connectDb();
 
+// it`s Get Api with mangodb
 export async function GET() {
     await mongoose.connect(process.env.MANGO_DB_DRIVERS, { dbName: 'productDB', })
     const data = await myProduct.find();
